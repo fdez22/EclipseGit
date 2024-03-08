994962f68489cc663a8317aa7c2c10d5bab456c2
ignacio <ignacio@gmail.com>
2024-03-08 11:11:25 +0100

    Cambio en rama A

--- GitModeloA/src/com/dam/a/ClaseEntornos.java
+++ GitModeloA/src/com/dam/a/ClaseEntornos.java
@@ -5,6 +5,7 @@ public class ClaseEntornos {
 	public static void main(String[] args) {
 	System.out.println("Hola Mundo");
 	System.out.println("Hola a todos");
+	System.out.println("Cambio rama a");
 	}
 
 }
